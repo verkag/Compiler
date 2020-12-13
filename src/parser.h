1f01a0fe530fcf9b6ad407b40e8ec29c6082a2ca
verkag <47256497+verkag@users.noreply.github.com>
2020-12-12 21:38:44 -0800

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -9,4 +9,24 @@ void Subtract();
 
 void Expression();
 
+void Assignment();
+
+void Multiply();
+
+void Divide();
+
+void Factor();
+
+bool IsAddop(char c);
+
+bool IsWhite(char c);
+
+bool IsAlNum(char c);
+
+void SkipWhite();
+
+void Ident();
+
+bool IsWhite(char c);
+
 #endif
